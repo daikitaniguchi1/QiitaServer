cdd458b532e8947f54b2c3c2ac9aa8c51fce9f15
daikitaniguchi1 <t.daiki50@gmail.com>
2014-08-18 21:23:22 +0900

    spec for Item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,23 @@
 require 'spec_helper'
 
 describe Item do
+
+  describe 'set_items_from_api' do
+    let(:item){Item.new}
+    let(:per_page){Settings.items[:per_page]}
+    let(:page){Settings.items[:page]}
+
+    before do
+      expect(Item).to receive(:get) {mock_response}
+      item.set_items_from_api
+    end
+    it { expect(Item.count()).to eq(1) }
+    it { expect(Item.all.pluck(:qiita_id).first).to eq(1) }
+    it { expect(Item.all.pluck(:data).first['url']).to eq('http://qiita.com') }
+  end
+
+  def mock_response
+    [{"id"=>1, "stock_count"=>100, "url"=>"http://qiita.com"}]
+  end
+
 end
