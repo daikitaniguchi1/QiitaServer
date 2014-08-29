4d7a02109cc4bf5049008533502e87f86eb07840
kidachi_ <t.daiki50@gmail.com>
2014-08-29 17:17:30 +0900

    pass spec

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Item do
 
   describe 'set_items_from_api' do
-    let(:item){Item.new}
     let(:per_page){Settings.items[:per_page]}
     let(:page){Settings.items[:page]}
 
     before do
       expect(Item).to receive(:get) {mock_response}
-      item.set_items_from_api
+      Item.set_items_from_api
     end
     it { expect(Item.count()).to eq(1) }
     it { expect(Item.all.pluck(:qiita_id).first).to eq(1) }
