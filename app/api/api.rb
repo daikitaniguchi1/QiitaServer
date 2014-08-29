a2e5f89ba044ee2ab3b1da38790453f82a8fac5b
daikitaniguchi1 <t.daiki50@gmail.com>
2014-08-29 11:00:05 +0900

    item api

---
+++ app/api/api.rb
@@ -0,0 +1,23 @@
+class API < Grape::API
+  format :json
+
+  helpers do
+
+    def error_handling!(message, code)
+      p message
+      Rails.logger.error("code:#{code}: #{message}")
+      error!({error: message}, code)
+    end
+
+  end
+
+  resource :items do
+    get do
+      begin
+        Item.all
+      rescue => e
+        error_handling!(e.message, 400)
+      end
+    end
+  end
+end
