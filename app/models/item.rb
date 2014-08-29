14e88d1109ef62dfe0b9251fcda3cb90fed14174
kidachi_ <t.daiki50@gmail.com>
2014-08-29 17:16:18 +0900

    change instance method to class method

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,35 +6,35 @@ class Item
   include HTTParty
   base_uri 'https://qiita.com/api/v1'
 
-  def set_items_from_api
-    1.step(Settings.items[:page], 1) do |index|
-      save_items_from(api_response(index))
+  class << self
+    def set_items_from_api
+      1.step(Settings.items[:page], 1) do |index|
+        save_items_from(api_response(index))
+      end
     end
-  end
 
-  private
-
-  def api_response(page)
-    self.class.get('/items.json',
-                   { query: {per_page:Settings.items[:per_page], page: page} }
-    )
-  end
-
-  def save_items_from(parsed_json)
-    parsed_json.each do |item|
-      next if is_exist?(item['id']) || under_threshold?(item['stock_count'])
-      Item.create(qiita_id: item['id'], data: item)
-      p "qiita_id #{item['id']} is saved!"
-      p item['url']
+    def api_response(page)
+      self.get('/items.json',
+                     { query: {per_page:Settings.items[:per_page], page: page} }
+      )
     end
-  end
 
-  def is_exist?(qiita_id)
-    Item.where(qiita_id: qiita_id).length > 0
-  end
+    def save_items_from(parsed_json)
+      parsed_json.each do |item|
+        next if is_exist?(item['id']) || under_threshold?(item['stock_count'])
+        Item.create(qiita_id: item['id'], data: item)
+        p "qiita_id #{item['id']} is saved!"
+        p item['url']
+      end
+    end
 
-  def under_threshold?(stock)
-    stock < Settings.items[:threshold]
+    def is_exist?(qiita_id)
+      Item.where(qiita_id: qiita_id).length > 0
+    end
+
+    def under_threshold?(stock)
+      stock < Settings.items[:threshold]
+    end
   end
 
 end
